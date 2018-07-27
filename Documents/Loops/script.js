757f43635e01723951de7ff70c5da5a940970ac4
DePaul <CONF0208@depaul.edu>
2018-07-27 13:14:59 -0500

    started but not finished

---
+++ Documents/Loops/script.js
@@ -0,0 +1,28 @@
+$(document).ready (function() {
+console.log("ready");
+
+
+		
+	var movies = ["Black Panther", "Avengers: Infinity War", "Incredibles 2"];
+	for (var i=0; i<movies.length; i++) {
+		$("#result").append("<h2>" + movies[i] + "</h2>");
+		}
+		
+		
+	for (var i=0; i<700000000; i++) {
+	}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	});
+
+
+	
